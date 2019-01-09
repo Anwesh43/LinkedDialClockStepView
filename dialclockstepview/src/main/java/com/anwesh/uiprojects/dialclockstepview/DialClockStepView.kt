0cd22a14d38764037954bee684956815427b7e23
Anwesh43 <anweshthecool0@gmail.com>
2019-01-09 08:58:13 +0530

    adding libraries and adding constants

--- dialclockstepview/src/main/java/com/anwesh/uiprojects/dialclockstepview/DialClockStepView.kt
+++ dialclockstepview/src/main/java/com/anwesh/uiprojects/dialclockstepview/DialClockStepView.kt
@@ -4,3 +4,19 @@ package com.anwesh.uiprojects.dialclockstepview
  * Created by anweshmishra on 09/01/19.
  */
 
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val dials : Int = 3
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.6f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#4527A0")
+val backColor : Int = Color.parseColor("#BDBDBD")
