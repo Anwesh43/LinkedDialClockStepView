f98def5b061ca660a393880a9e67775b026f08a3
Anwesh43 <anweshthecool0@gmail.com>
2019-01-09 09:32:24 +0530

    adding State class

--- dialclockstepview/src/main/java/com/anwesh/uiprojects/dialclockstepview/DialClockStepView.kt
+++ dialclockstepview/src/main/java/com/anwesh/uiprojects/dialclockstepview/DialClockStepView.kt
@@ -66,7 +66,7 @@ fun Canvas.drawDCNode(i : Int, scale : Float, paint : Paint) {
 class DialClockStepView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    
+
     override fun onDraw(canvas : Canvas) {
 
     }
@@ -78,5 +78,25 @@ class DialClockStepView(ctx : Context) : View(ctx) {
             }
         }
         return true
+    }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += scale.updateScale(dir, dials, 1)
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
     }
 }
