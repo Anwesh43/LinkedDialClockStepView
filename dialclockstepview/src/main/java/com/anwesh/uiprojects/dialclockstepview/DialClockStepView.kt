1693e4181ecf1c99aaf0fd7418abb83c70c10d3e
Anwesh43 <anweshthecool0@gmail.com>
2019-01-09 09:18:21 +0530

    adding drawDCNode canvas extension method

--- dialclockstepview/src/main/java/com/anwesh/uiprojects/dialclockstepview/DialClockStepView.kt
+++ dialclockstepview/src/main/java/com/anwesh/uiprojects/dialclockstepview/DialClockStepView.kt
@@ -27,3 +27,38 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorScale(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorScale(a, b) * dir * scGap
+
+fun Canvas.drawDial(x : Float, y : Float, r : Float, deg : Float, paint : Paint) {
+    save()
+    translate(x, y)
+    drawCircle(0f, 0f, r, paint)
+    rotate(deg)
+    drawLine(0f, 0f, 0f,-2 * r / 3, paint)
+    restore()
+
+}
+
+fun Canvas.drawDCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val gapDeg : Float = 360f / dials
+    paint.style = Paint.Style.STROKE
+    paint.strokeWidth = Math.min(w, h) / 60
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.color = foreColor
+    save()
+    translate(gap * (i + 1), h/2)
+    drawDial(0f, 0f, size, 360f * sc2, paint)
+    for (j in 0..(dials - 1)) {
+        val sc : Float = sc1.divideScale(j, dials)
+        save()
+        rotate(gapDeg / 4 + (gapDeg) * j)
+        drawDial(size, 0f, size/4, 360f * sc, paint)
+        restore()
+    }
+    restore()
+}
