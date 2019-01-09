06029f4f4116766118e3479c7e2673d9a23e9718
Anwesh43 <anweshthecool0@gmail.com>
2019-01-09 09:40:11 +0530

    adding DCNode class

--- dialclockstepview/src/main/java/com/anwesh/uiprojects/dialclockstepview/DialClockStepView.kt
+++ dialclockstepview/src/main/java/com/anwesh/uiprojects/dialclockstepview/DialClockStepView.kt
@@ -127,4 +127,48 @@ class DialClockStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DCNode(var i : Int, val state : State = State()) {
+
+        private var next : DCNode? = null
+        private var prev : DCNode? = null
+
+        fun addNeighbor() {
+            if (this.i < nodes - 1) {
+                next = DCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        init {
+            addNeighbor()
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDCNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DCNode {
+            var curr : DCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
